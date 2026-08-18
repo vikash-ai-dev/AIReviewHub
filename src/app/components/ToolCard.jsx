003c91a58c7bd7aa7dc5025d4c3e7e04a987239c
Vikash <patelvikash6777@gmail.com>
2026-08-18 17:34:22 +0530

    adding the link in view details

--- src/app/components/ToolCard.jsx
+++ src/app/components/ToolCard.jsx
@@ -1,19 +1,24 @@
+import Link from "next/link";
 export default function ToolCard({ tool }) {
   return (
     <article className="rounded-2xl border border-zinc-800 bg-zinc-900 p-6">
-        <h2 className="text-2xl font-bold text-white">{tool.name}</h2>
-        <p className="mt-3 text-zinc-400 leading-6">{tool.description}</p>
-        <div className="mt-5 space-y-5 text-sm text-zinc-400">
-          <p>Category: {tool.category}</p>
-          <p>Pricing: {tool.pricing}</p>
-          <p>Rating: {tool.rating}/5</p>
-          <p>Best For: {tool.bestFor}</p>
-        </div>
-        <button className="mt-6 w-full rounded-xl bg-violet-600 px-4 py-3 font-semibold text-white transition hover:bg-violet-500">
-          View Details
-        </button>
-        
+      <h2 className="text-2xl font-bold text-white">{tool.name}</h2>
+      <p className="mt-3 text-zinc-400 leading-6">{tool.description}</p>
+      <div className="mt-5 space-y-5 text-sm text-zinc-400">
+        <p>Category: {tool.category}</p>
+        <p>Pricing: {tool.pricing}</p>
+        <p>Rating: {tool.rating}/5</p>
+        <p>Best For: {tool.bestFor}</p>
+      </div>
       
+      <Link
+        href={`/tools/${tool.slug}`}
+        className="mt-6 block w-full rounded-xl bg-violet-600 px-4 py-3 text-center font-semibold text-white transition hover:bg-violet-500"
+      >
+        View Details
+      </Link>
+
+
     </article>
   );
 }
